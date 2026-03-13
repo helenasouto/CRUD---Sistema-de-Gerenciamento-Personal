e57490a7eb2e9634bc22a4629f7724383755898b
helenasouto <helena.souto@academico.com.br>
2026-03-13 01:58:10 -0300

    db: add prisma schema and migrations

---
+++ prisma/migrations/20260313042232_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `circunferenciaAbdomen` on the `avaliacoes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "avaliacoes" DROP COLUMN "circunferenciaAbdomen";
